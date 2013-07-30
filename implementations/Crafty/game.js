e783cbcc0b6a9a4a9f36f904c5272064ebf74b18
louisstow <louisstow@gmail.com>
2013-07-30 10:34:22 +1000

    * Display two ads

--- implementations/Crafty/game.js
+++ implementations/Crafty/game.js
@@ -70,49 +70,20 @@ function start () {
 	});
 
 	var adOptions = {
-    APP_ID: "Mozilla_AppTest_other",
-    PORTAL: 8471,
-    IS_MOBILE_WEB: true,
-    IS_ORMMA_SUPPORT: false,
-    IS_MRAID_SUPPORT: false,
-    IS_INTERSTITIAL_AD: true,
-    IMEI_MD5: "",
-    IMEI_SHA1: "",
-    MAC_MD5: "",
-    MAC_SHA1: "",
-    UDID_MD5: "",
-    UDID_SHA1: "",
-    IMSI_MD5: "",
-    IMSI_SHA1: "",
-    ANDROID_ID_MD5: "",
-    ANDROID_ID_SHA1: "",
-    IDFA: "",
-    IDFV: "",
-    CATEGORY: "Music",
-    AGE: "",
-    GENDER: "",
-    KEYWORDS: "Rock,Pop,Jazz,Blues",
-    LOCATION: "",
-    GPS_COORDINATES: "",
-    DEVICE_WIDTH: window.innerWidth, //resolution width of the devic,
-    DEVICE_HEIGHT: window.innerHeight, //resolution height of the devic,
-    MOBILE_NETWORK_CODE: "",
-    MOBILE_COUNTRY_CODE: "",
-    NETWORK: "", //values are 3G and WIF,
-    OPTIONAL_WIDTH: "", //optional ad widt,
-    OPTIONAL_HEIGHT: "", //optional ad heigh,
-    IMPRESSION_PIXEL: "",
-    CLICK_PIXEL: "",
-    FAILOVER: "",
-    REFRESH_RATE: 5
-};
+        APP_ID: "Mozilla_AppTest_other",
+        IS_INTERSTITIAL_AD: true,
+        FS: true
+    };
 
-	//create the Ad object based on the above options
+	//create a Fullsize ad
 	ad = Inneractive.createAd(adOptions);
 	ad
 	    .placement("bottom", "center")
-	    .setSize(320, 480)
 	    .addTo(document.body);
+
+    // create a smaller ad at bottom center
+    bottomAd = Inneractive.createAd({APP_ID: "Mozilla_AppTest_other"});
+    bottomAd.placement("bottom", "center").setSize(320, 50).addTo(document.body);
 }
 
 /**
